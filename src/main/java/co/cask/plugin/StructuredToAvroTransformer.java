0142bb4047cbe703c0285f122f2c77b9b13481dc
yaojiefeng <yaojie@cask.co>
2018-02-12 12:03:59 -0800

    add copyright and fix indentation

--- src/main/java/co/cask/plugin/StructuredToAvroTransformer.java
+++ src/main/java/co/cask/plugin/StructuredToAvroTransformer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright © 2018 Cask Data, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package co.cask.plugin;
 
 import co.cask.cdap.api.data.format.StructuredRecord;
@@ -15,48 +31,48 @@ import javax.annotation.Nullable;
  * Structured Record to Avro converter
  */
 public class StructuredToAvroTransformer extends AbstractStructuredRecordTransformer<GenericRecord> {
-	private final Map<Integer, Schema> schemaCache;
+  private final Map<Integer, Schema> schemaCache;
 
-	public StructuredToAvroTransformer(@Nullable co.cask.cdap.api.data.schema.Schema outputSchema) {
-		super(outputSchema);
-		this.schemaCache = Maps.newHashMap();
-	}
+  public StructuredToAvroTransformer(@Nullable co.cask.cdap.api.data.schema.Schema outputSchema) {
+    super(outputSchema);
+    this.schemaCache = Maps.newHashMap();
+  }
 
-	@Override
-	public GenericRecord transform(StructuredRecord structuredRecord,
-																 co.cask.cdap.api.data.schema.Schema schema) throws IOException {
-		co.cask.cdap.api.data.schema.Schema structuredRecordSchema = structuredRecord.getSchema();
+  @Override
+  public GenericRecord transform(StructuredRecord structuredRecord,
+                                 co.cask.cdap.api.data.schema.Schema schema) throws IOException {
+    co.cask.cdap.api.data.schema.Schema structuredRecordSchema = structuredRecord.getSchema();
 
-		Schema avroSchema = getAvroSchema(schema);
+    Schema avroSchema = getAvroSchema(schema);
 
-		GenericRecordBuilder recordBuilder = new GenericRecordBuilder(avroSchema);
-		for (Schema.Field field : avroSchema.getFields()) {
-			String fieldName = field.name();
-			co.cask.cdap.api.data.schema.Schema.Field schemaField = structuredRecordSchema.getField(fieldName);
-			if (schemaField == null) {
-				throw new IllegalArgumentException("Input record does not contain the " + fieldName + " field.");
-			}
-			recordBuilder.set(fieldName, convertField(structuredRecord.get(fieldName), schemaField.getSchema()));
-		}
-		return recordBuilder.build();
-	}
+    GenericRecordBuilder recordBuilder = new GenericRecordBuilder(avroSchema);
+    for (Schema.Field field : avroSchema.getFields()) {
+      String fieldName = field.name();
+      co.cask.cdap.api.data.schema.Schema.Field schemaField = structuredRecordSchema.getField(fieldName);
+      if (schemaField == null) {
+        throw new IllegalArgumentException("Input record does not contain the " + fieldName + " field.");
+      }
+      recordBuilder.set(fieldName, convertField(structuredRecord.get(fieldName), schemaField.getSchema()));
+    }
+    return recordBuilder.build();
+  }
 
-	private Schema getAvroSchema(co.cask.cdap.api.data.schema.Schema cdapSchema) {
-		int hashCode = cdapSchema.hashCode();
-		if (schemaCache.containsKey(hashCode)) {
-			return schemaCache.get(hashCode);
-		} else {
-			Schema avroSchema = new Schema.Parser().parse(cdapSchema.toString());
-			schemaCache.put(hashCode, avroSchema);
-			return avroSchema;
-		}
-	}
+  private Schema getAvroSchema(co.cask.cdap.api.data.schema.Schema cdapSchema) {
+    int hashCode = cdapSchema.hashCode();
+    if (schemaCache.containsKey(hashCode)) {
+      return schemaCache.get(hashCode);
+    } else {
+      Schema avroSchema = new Schema.Parser().parse(cdapSchema.toString());
+      schemaCache.put(hashCode, avroSchema);
+      return avroSchema;
+    }
+  }
 
-	@Override
-	protected Object convertBytes(Object field) {
-		if (field instanceof ByteBuffer) {
-			return field;
-		}
-		return ByteBuffer.wrap((byte[]) field);
-	}
+  @Override
+  protected Object convertBytes(Object field) {
+    if (field instanceof ByteBuffer) {
+      return field;
+    }
+    return ByteBuffer.wrap((byte[]) field);
+  }
 }
