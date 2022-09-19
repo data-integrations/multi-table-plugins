9e2e122a54387185f049dfa9d9e6d0ebc50ae06a
itsankit-google <itsankitjain@google.com>
2022-09-19 16:37:47 +0530

    PLUGIN-1290: removing hive jdbc from plugin tooltips and markdown

--- src/main/java/io/cdap/plugin/format/MultiTableConf.java
+++ src/main/java/io/cdap/plugin/format/MultiTableConf.java
@@ -78,10 +78,8 @@ public class MultiTableConf extends PluginConfig {
 
   @Macro
   @Nullable
-  @Description("Whether to enable auto commit for queries run by this source. Defaults to false. " +
-    "This setting should only matter if you are using a jdbc driver that does not support a false value for " +
-    "auto commit, or a driver that does not support the commit call. For example, the Hive jdbc driver will throw " +
-    "an exception whenever a commit is called. For drivers like that, this should be set to true.")
+  @Description("Whether to enable auto-commit for queries run by this source. In most cases, set to false.\n" +
+    "If you use a JDBC driver that results in an error when the commit operation is run, set to 'true'.")
   private Boolean enableAutoCommit;
 
   @Nullable
